d8de8ade760a5c2bfda56f9dc484a8d7772f5750
Shiva Gupta <86485099+shiva5152@users.noreply.github.com>
2023-11-27 19:31:19 +0530

    Update sendToken.ts

--- src/utils/sendToken.ts
+++ src/utils/sendToken.ts
@@ -25,9 +25,8 @@ export const sendToken = (user: any, statusCode: number, res: Response, accessTo
   // one day=24*60*60*1000
   const options = {
     httpOnly: process.env.NODE_ENV === 'production',
-    sameSite: none,
     expires: new Date(Date.now() + 60 * 24 * 60 * 60 * 1000),
-    // secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === 'production',
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
